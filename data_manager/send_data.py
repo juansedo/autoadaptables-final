5cf93d3a64f22f97319a43975c5365b17b7fb2e1
zaphod9801 <71454879+zaphod9801@users.noreply.github.com>
2024-05-24 20:15:13 -0500

    merge conflicts to be fixed (#2)
    * gen 1 added, missing serial communication and CNY sensors
    
    * CNY sensors added
    
    * gen1 finished, pending serial com test
    
    * requirements.txt added
    
    * linear regression added for CO2
    
    * constants added
    
    ---------
    
    Co-authored-by: JohanRuiz <jhoan.ruiz@chiper.co>

--- data_manager/send_data.py
+++ data_manager/send_data.py
@@ -7,19 +7,41 @@ from constants import (
     URL,
     TIME_URL,
 )
+from dateutil import parser
+from datetime import datetime, timedelta
+from sklearn.linear_model import LinearRegression
+import numpy as np
 
 ser = serial.Serial(SERIAL_PORT, 9600)
+ser = serial.Serial('/dev/ttyS0', 9600)  
+url = 'https://isa.requestcatcher.com/post'
+
+model = LinearRegression()
+
+# Datos de entrenamiento iniciales (simulados)
+X_train = np.array([[400], [300], [200], [100], [50]])  # Valores de CO2
+y_train = np.array([120, 90, 60, 30, 15])  # Tiempos de espera en segundos
+
+model.fit(X_train, y_train)
 
 def get_time_of_day():
     response = requests.get(TIME_URL)
+    response = requests.get("http://worldtimeapi.org/api/timezone/Etc/UTC")
     data = response.json()
     current_time = datetime.fromisoformat(data["datetime"][:-1])
     hour = current_time.hour
-
-    if 6 <= hour < 18:
-        return "DAY"
+ 
+    if 'datetime' in data:
+        current_time = parser.isoparse(data["datetime"])
+        current_time = current_time - timedelta(hours=5)
+        hour = current_time.hour
+ 
+        if 6 <= hour < 18:
+            return "DAY"
+        else:
+            return "NIGHT"
     else:
-        return "NIGHT"
+        raise ValueError("La respuesta de la API no contiene la clave 'datetime'")
 
 while True:
     try:
@@ -30,8 +52,20 @@ while True:
         
         line = ser.readline().decode('utf-8').strip()
         print("Datos leídos del Arduino:", line)
-        
+
         response = requests.post(URL, data={'sensor_data': line})
+        
+        # Extraer el valor del sensor de CO2 de los datos leídos del Arduino
+        sensor_data = dict(item.split(": ") for item in line.split(", "))
+        co2_value = int(sensor_data.get("CO2", 0))
+        
+        # Predecir el tiempo de espera del semáforo usando el modelo de regresión
+        predicted_wait_time = model.predict(np.array([[co2_value]]))[0]
+        
+        # Enviar el tiempo de espera predicho al Arduino
+        ser.write(f"WAIT:{predicted_wait_time:.2f}\n".encode('utf-8'))
+        
+        response = requests.post(url, data={'sensor_data': line, 'predicted_wait_time': predicted_wait_time})
         print("Respuesta del servidor:", response.status_code, response.text)
         
         time.sleep(60)
